0b725b5dfc9ef77413478eff84a4c35d77c9cdac
Konstantinos Siampanis <siampako@aris-lux.lan>
2018-05-16 18:44:16 +0300

    Add provider for Traxsource

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -1,23 +1,30 @@
 const mongoose = require('mongoose');
 const Track = require('../api/models/track.model');
 const Beatport = require('./providers/beatport.provider');
+const Traxsource = require('./providers/traxsource.provider');
 
 mongoose.connect('mongodb://localhost/albdb');
-const beatport = new Beatport();
 
+const beatport = new Beatport();
+const traxsource = new Traxsource();
+
+const providers = [beatport, traxsource];
 
 const go = async () => {
     try {
-        const meta = await beatport.parser();
-        const options = {upsert: true, new: true, setDefaultsOnInsert: true};
-        for (let piece of meta) {
-            const query = {title: piece.title, artist: piece.artist, category: piece.category};
-            // TODO: Change this so data is inserted only if not found
-            // findOneAndUpdate will update every document and that prevents
-            // playlist updater to find tracks were recently changed (uri added)
-            await Track.findOneAndUpdate(query, {styles: piece.styles}, options);
+        for (let provider of providers) {
+            const meta = await provider.parser();
+            const options = {upsert: true, new: true, setDefaultsOnInsert: true};
+            for (let piece of meta) {
+                const query = {title: piece.title, artist: piece.artist, category: piece.category};
+                // TODO: Change this so data is inserted only if not found
+                // findOneAndUpdate will update every document and that prevents
+                // playlist updater to find tracks were recently changed (uri added)
+                await Track.findOneAndUpdate(query, {styles: piece.styles}, options);
+            }
+            console.log(`Done parsing ${meta.length} titles from ${provider.constructor.name}`);
         }
-        console.log(`Done parsing ${meta.length} titles.`);
+        console.log('The end.');
     }
     catch (error) {
         console.log(error);
