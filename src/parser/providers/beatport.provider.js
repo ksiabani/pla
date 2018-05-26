66f65e9188ac2dcf93056160147ec68517b1ef2c
Kostas Siabanis <kostas.siabanis@gmail.com>
2018-05-26 11:01:42 +0300

    Introduce follow up scenarios for Beatport, add one for House

--- src/parser/providers/beatport.provider.js
+++ src/parser/providers/beatport.provider.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const sleep = require('../../utils/sleep');
+const root = 'https://www.beatport.com';
 
 const scenarios = [
     {
@@ -25,30 +26,6 @@ const scenarios = [
             });
         }
     },
-    // Terrible scenario due to bad quality of collections featuring old stuff
-    // Commenting out for now, will replace with a new scenario soon...
-    // {
-    //     name: 'New House',
-    //     url: 'https://www.beatport.com/genre/house/5/tracks?per-page=150&page=',
-    //     pagesToFollow: 1,
-    //     parserFn: ($) => {
-    //         return Array.from($('.bucket-item.ec-item.track'), el => {
-    //             let styles = [];
-    //             const mainTitle = $(el).data('ec-name');
-    //             const artists = $(el).data('ec-d1').split(',');
-    //             styles.push($(el).data('ec-d3'));
-    //             if ($(el).data('ec-d4')) {
-    //                 styles.push($(el).data('ec-d4'));
-    //             }
-    //             let remixTitle = $(el).find('.buk-track-remixed').text();
-    //             if (remixTitle.toLowerCase() === 'original mix') {
-    //                 remixTitle = ''
-    //             }
-    //             const title = remixTitle ? `${mainTitle} ${remixTitle}` : mainTitle;
-    //             return {title, artists, styles, category: 'new'};
-    //         });
-    //     }
-    // },
     {
         name: 'New Electronica / Downtempo',
         url: 'https://www.beatport.com/tracks/all?type=Release&per-page=150&genres=3&page=',
@@ -70,6 +47,34 @@ const scenarios = [
                 return {title, artists, styles, category: 'new'};
             });
         }
+    },
+    {
+        name: 'Featured Latest House',
+        url: 'https://www.beatport.com/genre/house/5',
+        parserFn: ($) => {
+            return Array.from($('.bucket-item.ec-item.release'), el => {
+                return $(el).find('.release-artwork-parent').attr('href');
+            });
+        },
+        scenarioToFollow: {
+            parserFn: ($) => {
+                return Array.from($('.bucket-item.ec-item.track'), el => {
+                    let styles = [];
+                    const mainTitle = $(el).data('ec-name');
+                    const artists = $(el).data('ec-d1').split(',');
+                    styles.push($(el).data('ec-d3'));
+                    if ($(el).data('ec-d4')) {
+                        styles.push($(el).data('ec-d4'));
+                    }
+                    let remixTitle = $(el).find('.buk-track-remixed').text();
+                    if (remixTitle.toLowerCase() === 'original mix') {
+                        remixTitle = ''
+                    }
+                    const title = remixTitle ? `${mainTitle} ${remixTitle}` : mainTitle;
+                    return {title, artists, styles, category: 'new'};
+                });
+            }
+        }
     }
 ];
 
@@ -81,14 +86,35 @@ class Beatport {
         try {
             let meta = [];
             for (let scenario of scenarios) {
-                for (let i = 1; i <= scenario.pagesToFollow; i++) {
-                    const url = `${scenario.url}${i}`;
+
+                // Scenario has pagination
+                if (scenario.pagesToFollow) {
+                    for (let i = 1; i <= scenario.pagesToFollow; i++) {
+                        const url = `${scenario.url}${i}`;
+                        const response = await axios.get(url);
+                        const $ = cheerio.load(response.data);
+                        const tracks = scenario.parserFn($);
+                        meta = [...meta, ...tracks];
+                        console.log(`Running '${scenario.name}' (page ${i} of ${scenario.pagesToFollow}) from Beatport`);
+                        await sleep(10000);
+                    }
+                }
+
+                // Scenario has follow up scenario
+                if (scenario.scenarioToFollow) {
+                    const url = `${scenario.url}`;
                     const response = await axios.get(url);
                     const $ = cheerio.load(response.data);
-                    const tracks = scenario.parserFn($);
-                    meta = [...meta, ...tracks];
-                    console.log(`Running '${scenario.name}' (page ${i} of ${scenario.pagesToFollow}) from Beatport`);
-                    await sleep(10000);
+                    const followUpUrls = scenario.parserFn($);
+                    for (let i = 0; i < followUpUrls.length; i++) {
+                        const url = root + followUpUrls[i];
+                        const response = await axios.get(url);
+                        const $ = cheerio.load(response.data);
+                        const tracks = scenario.scenarioToFollow.parserFn($);
+                        meta = [...meta, ...tracks];
+                        console.log(`Running '${scenario.name}' (with follow ups) from Beatport`);
+                        await sleep(10000);
+                    }
                 }
             }
             return meta;
