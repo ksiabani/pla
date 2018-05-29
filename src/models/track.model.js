524dd55dad4ce10c334457e7a6efc6681987df2c
Konstantinos Siampanis <kostas.siabanis@gmail.com>
2018-05-29 11:37:24 +0300

    Add routes and logic for tracks endpoint

--- src/models/track.model.js
+++ src/models/track.model.js
@@ -15,22 +15,82 @@ const trackSchema = new Schema({
     lastScannedAt: Date // last time a match was attempted
 });
 
-// assign a function to the "methods" object of our animalSchema
-// trackSchema.methods.findSimilarTypes = function(cb) {
-//     return this.model('Animal').find({ type: this.type }, cb);
-// };
-
+// Randomly get a 100 tracks that were previously searched for, but not found on Spotify
 trackSchema.statics.getRandomScannedNotMatched = function () {
     return this.model('Track')
-        .aggregate([{$match: {spotify_uri: null, lastScannedAt: {$ne: null}}}])
+        .aggregate([
+            {
+                $match: {
+                    spotify_uri: null,
+                    lastScannedAt: {
+                        $ne: null
+                    }
+                }
+            }
+        ])
         .sample(100)
         .exec();
 };
 
+// Get tracks that have not been searched for on Spotify, limit to a 100 to avoid rate limiting
 trackSchema.statics.getNotScanned = function () {
     return this.model('Track')
-        .find({spotify_uri: null, lastScannedAt: null}, {}, {lean: true})
+        .find({
+            spotify_uri: null,
+            lastScannedAt: null
+        }, {}, {lean: true})
         .limit(100)
+        .exec();
+};
+
+// Get all tracks that have been found on Spotify
+// TODO: limit results, add pagination
+// TODO: Sort by release date
+trackSchema.statics.getTracks = function () {
+    return this.model('Track')
+        .find({
+            spotify_uri: {
+                $ne: null
+            }
+        })
+        .exec();
+};
+
+// Get all tracks that have been found on Spotify
+trackSchema.statics.getTrack = function (trackId) {
+    return this.model('Track')
+        .findOne({
+            _id: trackId
+        })
+        .exec();
+};
+
+// Get new tracks that have been found on Spotify
+// TODO: limit results, add pagination
+trackSchema.statics.getNewTracks = function (options) {
+    return this.model('Track')
+        .find({
+            category: 'new',
+            spotify_uri: {
+                $ne: null
+            },
+            styles: { $in: options.styles }
+        })
+        .exec();
+};
+
+// Get new tracks that have been found on Spotify
+// TODO: limit results, add pagination
+// TODO: Test this when tops are added
+trackSchema.statics.getTopTracks = function () {
+    return this.model('Track')
+        .find({
+            category: 'top',
+            spotify_uri: {
+                $ne: null
+            },
+            styles: { $in: options.styles }
+        })
         .exec();
 };
 
