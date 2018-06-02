db5db47cf1eec068d12d20d85bfd76ed8d7c6a18
Kostas Siabanis <kostas.siabanis@gmail.com>
2018-06-03 00:11:07 +0300

    Tide up matcher

--- src/services/db.service.js
+++ src/services/db.service.js
@@ -13,3 +13,6 @@ module.exports = mongodbUri => ({
 // MongoDBs pool size (default 5). If you want to start another connection to another server (e.g test) you can do it
 // through calling this service
 
+// TODO: Do you really need this service?
+// You could just use mongoose.connect(uri) where you need to, instead of db.connect(uri)
+// http://www.albertgao.xyz/2017/06/19/test-your-model-via-jest-and-mongoose/
