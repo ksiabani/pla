d757af7f364f5800e90930251d64c2d6a1bc89b9
Konstantinos Siampanis <kostas.siabanis@gmail.com>
2018-06-11 09:41:19 +0300

    Add scenario for chill out

--- src/parser/providers/traxsource.provider.js
+++ src/parser/providers/traxsource.provider.js
@@ -107,6 +107,27 @@ const scenarios = [
                 return {title, artists, styles, category: 'new'};
             });
         }
+    },
+    {
+        name: 'Featured Lounge / Chill Out',
+        url: 'https://www.traxsource.com/genre/1/lounge-chill-out/featured?cn=tracks&ipp=100&gf=1&page=',
+        pagesToFollow: 1,
+        parserFn: ($) => {
+            return Array.from($('.trk-row.play-trk'), el => {
+                let styles = [];
+                const mainTitle = $(el).find('.trk-cell.title a').text();
+                let remixTitle = $(el).find('.trk-cell.title .version').clone().children().remove().end().text().replace(/\r?\n|\r/, '').trim();
+                // TODO: This will return comma separated values, remove join if you want back an array
+                const artists = Array.from($(el).find('.trk-cell.artists').find('.com-artists')).map(artist => $(artist).text());
+                const genre = $(el).find('.trk-cell.genre a').text();
+                if (remixTitle.toLowerCase() === 'original mix') {
+                    remixTitle = ''
+                }
+                const title = remixTitle ? `${mainTitle} ${remixTitle}` : mainTitle;
+                styles.push(genre);
+                return {title, artists, styles, category: 'new'};
+            });
+        }
     }
 ];
 
